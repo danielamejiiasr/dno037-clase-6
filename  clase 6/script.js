e64d468b24596cf9a9552af1aaa5bd3a1afcb441
danielamejiiasr <75910897+danielamejiiasr@users.noreply.github.com>
2022-04-13 13:07:35 -0400

    Delete script.js

---  clase 6/script.js
+++
@@ -1,2 +0,0 @@
-const menu = '<select onchange="location=this.value;"><option>Menu</option><option value="index.html">Portada</option><option value="ejemplo1.html">Ejemplo 1</option><option value="ejemplo2.html">Ejemplo 2</option><option value="ejemplo3.html">Ejemplo 3</option><option value="ejemplo4-1.html">Ejemplo 4</option><option value="ejemplo5.html">Ejemplo 5</option></select>';
-document.querySelector("body").innerHTML += menu;
